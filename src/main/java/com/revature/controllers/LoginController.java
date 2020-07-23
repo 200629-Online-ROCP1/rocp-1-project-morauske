dcf72c047f41875b0cc28bc4fe0836dd957ebb7c
Tim Morauske <tsm.mail00@gmail.com>
2020-07-23 12:40:48 -0600

    added checking password

--- src/main/java/com/revature/controllers/LoginController.java
+++ src/main/java/com/revature/controllers/LoginController.java
@@ -36,10 +36,11 @@ public class LoginController {
 		
 		// ls.login() will reach out the the DBase to verify username/password
 		User usr = ls.login(l);
-		
+		// check password
 		if(usr != null) {
 			//Note - the no args getSession method will create a new session if one does not already exist for this client. 
 			System.out.println("LoginController SESSION: setting user, loggedin and userType");
+			if (usr.getPassword().equals(l.password)) {
 			HttpSession ses = req.getSession();
 
 			ses.setAttribute("user", l);
@@ -48,8 +49,13 @@ public class LoginController {
 			ses.setAttribute("userType", usr.getRole().getRole());  // Getting the string form for Role eg. Admin,
 
 			res.setStatus(200);
-			//res.getWriter().println(((LoginDTO) sess.getAttribute("user")).username + " Login Successful!");
 			res.getWriter().println(l.username + " Login Successful!");
+			}
+			else {
+				res.setStatus(400);
+				res.getWriter().println("Invalid Credentials");
+				
+			}
 		} else {
 			//will only return a session if one is already associated with the request, will not create a new one. 
 			System.out.println("LoginController else invalidate session");
